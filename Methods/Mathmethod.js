2b280fd2104fde8e8012d7ef6b4f6522ca525b91
Man Prajapati <138698017+manprajapati77@users.noreply.github.com>
2024-05-21 09:12:48 +0000

    create a custmize method to round of numbers

--- Methods/Mathmethod.js
+++ Methods/Mathmethod.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+function round(number){
+    const int = parseInt(number)
+    const decimalNum = number - int
+
+    if (decimalNum >= 0.5){
+        return int + 1;
+        }
+        
+    else {
+        return int
+    }
+}
+console.log(round(0))
