1fa3c0a518a24aeb0ffe9b6219f2ec448d8e1b75
Man Prajapati <138698017+manprajapati77@users.noreply.github.com>
2024-05-21 09:24:52 +0000

    creat customize Math.abs method

--- Methods/Mathmethod.js
+++ Methods/Mathmethod.js
@@ -1,3 +1,7 @@
+
+//1. Math.round()
+
+/*
 function round(number){
     const int = parseInt(number)
     const decimalNum = number - int
@@ -10,4 +14,17 @@ function round(number){
         return int
     }
 }
-console.log(round(0))
+console.log(round(0))*/
+
+
+//2. Math.abs()
+
+function create_abs_method(num1){
+    if (num1 < 0){
+        return -num1
+    }
+    else {
+        return num1
+    }
+}
+console.log(create_abs_method(1.0054))
